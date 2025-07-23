0bd54e0c2546430c52513589fd28a8cd8f63550f
Karl <luigiloza@gmail.com>
2025-07-23 23:43:52 +0800

    feature engineering complete

---
+++ 3_Feature_Engineering_P2.py
@@ -0,0 +1,16 @@
+from helpers.constants import CONTINUOUS_RANGE, TARGETS_RANGE
+from ml_tools.utilities import save_dataframe, yield_dataframes_from_dir
+from ml_tools.data_exploration import clip_outliers_multi
+from paths import PM
+
+
+def main():
+    clip_dict = CONTINUOUS_RANGE | TARGETS_RANGE
+
+    for df, df_name in yield_dataframes_from_dir(datasets_dir=PM["feature engineering unclip"]):
+        df_clip = clip_outliers_multi(df=df, clip_dict=clip_dict, verbose=True)
+        save_dataframe(df=df_clip, save_dir=PM["feature engineering clip"], filename=df_name + "_clip")
+        
+
+if __name__ == "__main__":
+    main()
