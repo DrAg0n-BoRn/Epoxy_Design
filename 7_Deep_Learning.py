466227c03ebdd6fbbef5aa28872a415a0737e675
Karl <luigiloza@gmail.com>
2025-07-28 20:31:52 +0800

    Create ML training and optimization scripts
    delete ensemble based scripts

---
+++ 7_Deep_Learning.py
@@ -0,0 +1,103 @@
+from paths import PM
+from helpers.constants import MODEL_HIDDEN_LAYERS, INITIAL_LEARNING_RATE, DROP_OUT_RATE
+import pandas
+
+from ml_tools.path_manager import make_fullpath
+from ml_tools.utilities import yield_dataframes_from_dir
+from ml_tools import custom_logger
+
+from ml_tools.ML_datasetmaster import SimpleDatasetMaker
+from ml_tools.ML_models import MultilayerPerceptron
+from ml_tools.ML_callbacks import EarlyStopping, ModelCheckpoint, LRScheduler
+from ml_tools.ML_trainer import MyTrainer
+from ml_tools.keys import LogKeys
+
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim import Adam
+from torch.nn import MSELoss
+
+
+def single_run(df: pandas.DataFrame, df_name: str):
+    # Local directory
+    local_dir = make_fullpath(PM["model metrics"] / df_name, make=True, enforce="directory")
+    
+    # Make dataset
+    current_dataset = SimpleDatasetMaker(pandas_df=df)
+    
+    # Define neural network architecture
+    model = MultilayerPerceptron(in_features=len(current_dataset.feature_names),
+                                out_targets=1,
+                                hidden_layers=MODEL_HIDDEN_LAYERS,
+                                drop_out=DROP_OUT_RATE)
+    
+    # define loss function
+    loss_function = MSELoss()
+    
+    # Define optimizer
+    optimizer = Adam(model.parameters(), lr=INITIAL_LEARNING_RATE)
+    
+    # define LR scheduler
+    reduce_lr_on_plateau = ReduceLROnPlateau(optimizer=optimizer, mode='min')
+    
+    # Define callbacks
+    callback_early_stop = EarlyStopping(monitor=LogKeys.VAL_LOSS,
+                                        mode='min')
+    
+    callback_checkpoint = ModelCheckpoint(save_dir=local_dir,
+                                        save_best_only=True)
+    
+    callback_scheduler = LRScheduler(scheduler=reduce_lr_on_plateau,
+                                    monitor=LogKeys.VAL_LOSS)
+    
+    all_callbacks = [
+        callback_early_stop,
+        callback_checkpoint,
+        callback_scheduler
+    ]
+    
+    # Define Trainer
+    trainer = MyTrainer(model=model,
+                        train_dataset=current_dataset.train_dataset,
+                        test_dataset=current_dataset.test_dataset,
+                        kind="regression",
+                        criterion=loss_function,
+                        optimizer=optimizer,
+                        device="cuda:0",
+                        dataloader_workers=4,
+                        callbacks=all_callbacks)
+    
+    # Train model
+    history_log = trainer.fit(epochs=200,
+                              batch_size=10,
+                              shuffle=True)
+    
+    # Evaluate
+    trainer.evaluate(save_dir=local_dir)
+    
+    # Explain
+    trainer.explain(save_dir=local_dir,
+                    feature_names=current_dataset.feature_names)
+    
+    # save log
+    log_dict = {
+        "Task": "regression",
+        "Model Architecture": repr(model),
+        "Drop-out Rate": DROP_OUT_RATE,
+        "Dataframe": df_name,
+        "Target": current_dataset.target_name,
+        "Number of Features": len(current_dataset.feature_names),
+        "Features": current_dataset.feature_names
+    }
+    
+    custom_logger(data=log_dict | history_log,
+                  save_directory=local_dir,
+                  log_name=df_name)
+
+
+def main():
+    for df, df_name in yield_dataframes_from_dir(datasets_dir=PM["model datasets"]):
+        single_run(df, df_name)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 8_Optimization.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+from typing import Union
+
+from paths import PM
+from helpers.constants import DROP_OUT_RATE, MODEL_HIDDEN_LAYERS, CONTINUOUS_RANGE_OPTIMAL, NUMBER_FEATURES, NUMBER_BINARY_FEATURES
+
+from ml_tools.utilities import deserialize_object
+from ml_tools.path_manager import list_files_by_extension
+from ml_tools.ML_models import MultilayerPerceptron
+from ml_tools.ML_inference import PyTorchInferenceHandler
+from ml_tools.ML_optimization import create_pytorch_problem, run_optimization
+from ml_tools.optimization_tools import parse_lower_upper_bounds, plot_optimal_feature_distributions
+
+
+# --- Global Variables ---
+# Bounds
+lower_upper_tuple = parse_lower_upper_bounds(source=CONTINUOUS_RANGE_OPTIMAL)
+
+# feature names
+feature_names: list[str] = deserialize_object(PM["feature columns"]) # type: ignore
+
+
+# --- single function ---
+def single_model_optimization(state_dict_path: Union[str,Path], target_name: str, number_generations: int=500, repetitions: int=100):
+    # Model architecture
+    model = MultilayerPerceptron(in_features=NUMBER_FEATURES,
+                                out_targets=1,
+                                hidden_layers=MODEL_HIDDEN_LAYERS,
+                                drop_out=DROP_OUT_RATE)
+
+    # Define pytorch handler
+    pytorch_handler = PyTorchInferenceHandler(model=model,
+                                            state_dict=state_dict_path,
+                                            task="regression",
+                                            device="cuda:0")
+
+    # Define problem and searcher
+    torch_problem, torch_searcher = create_pytorch_problem(handler=pytorch_handler,
+                                                        bounds=lower_upper_tuple,
+                                                        binary_features=NUMBER_BINARY_FEATURES,
+                                                        task="maximize")
+    
+    # Run optimization
+    run_optimization(problem=torch_problem,
+                    searcher=torch_searcher,
+                    num_generations=number_generations,
+                    target_name=target_name,
+                    binary_features=NUMBER_BINARY_FEATURES,
+                    save_dir=PM["optimization results"],
+                    save_format="both",
+                    feature_names=feature_names,
+                    repetitions=repetitions)
+
+
+# --- Plot function ---
+def plot():
+    plot_optimal_feature_distributions(results_dir=PM["optimization results"],
+                                       save_dir=PM["optimization results"])
+
+
+# --- Batch function ---
+def main():
+    #RENAME state dictionary filenames to target names
+    model_weights_dict = list_files_by_extension(directory=PM["optimization models"], extension="pth")
+    
+    for target_filename, fpath in model_weights_dict.items():
+        single_model_optimization(state_dict_path=fpath,
+            target_name=target_filename,
+            number_generations=500,
+            repetitions=100)
+
+if __name__ == "__main__":
+    #NOTE: RENAME state dictionary filenames to target names
+    main()
+    # plot()
