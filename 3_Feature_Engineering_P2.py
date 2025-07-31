324262100d182e0995e13ef0da490ecbc6c2591d
Karl <luigiloza@gmail.com>
2025-07-31 12:41:41 +0800

    Update 3_Feature_Engineering_P2.py
    add type ignore

--- 3_Feature_Engineering_P2.py
+++ 3_Feature_Engineering_P2.py
@@ -10,7 +10,7 @@ def main():
 
     for df, df_name in yield_dataframes_from_dir(datasets_dir=PM["feature engineering unclip"]):
         # clip values
-        df_clip = clip_outliers_multi(df=df, clip_dict=clip_dict, verbose=True)
+        df_clip = clip_outliers_multi(df=df, clip_dict=clip_dict, verbose=True) # type: ignore
         # plot final value distributions
         plot_value_distributions(df=df_clip, save_dir=PM["feature engineering clip"])
         # save data
