dad759a6f45cb3bf35b05aff8d51099d5c02037d
Karl <luigiloza@gmail.com>
2025-12-10 18:12:19 +0800

    Update constants.py
    working on project updates

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,43 +1,36 @@
-### Raw data
-RAW_TARGETS = [
-    "断裂韧性",
-    "弯曲强度",
-    "弯曲模量",
-    "冲击强度",
-    "杨氏模量",
-    "拉伸强度",
-    "剪切强度",
-    "断裂伸长率"
-]
+# Raw targets map
+RAW_TARGETS = {
+    "断裂韧性": "Fracture Toughness(MPa m0.5)", # Missing 91% of data (REMOVED)
+    "弯曲强度": "Flexural Strength(MPa)",   # Missing 51% of data
+    "弯曲模量": "Flexural Modulus(MPa)",    # Missing 74% of data (REMOVED)
+    "冲击强度": "Impact Strength(kJ/m2)",   # Missing 64% of data
+    "杨氏模量": "Young Modulus(MPa)",   # Missing 69% of data (REMOVED)
+    "拉伸强度": "Tensile Strength(MPa)",    # Missing 23% of data
+    "剪切强度": "Shear Strength(MPa)", # Missing 94% of data (REMOVED)
+    "断裂伸长率": "Elongation at Break(%)"  # Missing 63% of data
+}
+
+# Cleaned target names
+TARGET_flexural_strength = "Flexural Strength(MPa)"
+TARGET_impact_strength = "Impact Strength(kJ/m2)"
+TARGET_tensile_strength = "Tensile Strength(MPa)"
+TARGET_elongation_at_break = "Elongation at Break(%)"
 
 TARGETS = [
-    "Fracture Toughness(MPa m0.5)",  #Dropped
-    "Flexural Strength(MPa)",
-    "Flexural Modulus(MPa)",
-    "Impact Strength(kJ/m2)",
-    "Young Modulus(MPa)",
-    "Tensile Strength(MPa)",
-    "Shear Strength(MPa)", #Dropped
-    "Elongation at Break(%)"
-]
-
-OPTIMIZATION_TARGETS = [
-    "Flexural Strength(MPa)",
-    "Flexural Modulus(MPa)",
-    "Impact Strength(kJ/m2)",
-    "Young Modulus(MPa)",
-    "Tensile Strength(MPa)",
-    "Elongation at Break(%)"
+    TARGET_flexural_strength,
+    TARGET_impact_strength,
+    TARGET_tensile_strength,
+    TARGET_elongation_at_break
 ]
 
 ### Value ranges
 TARGETS_RANGE = {
-    "Flexural Strength(MPa)": (0,7400),
-    "Flexural Modulus(MPa)": (0,200000),
-    "Impact Strength(kJ/m2)": (0,400),
-    "Young Modulus(MPa)": (0,300000),
-    "Tensile Strength(MPa)": (0,6000),
-    "Elongation at Break(%)": (0,400)
+    TARGET_flexural_strength: (0,7400),
+    # TARGET_flexural_modulus: (0,200000),
+    TARGET_impact_strength: (0,400),
+    # TARGET_young_modulus: (0,300000),
+    TARGET_tensile_strength: (0,6000),
+    TARGET_elongation_at_break: (0,400)
 }
 
 CONTINUOUS_RANGE = {
@@ -53,21 +46,3 @@ OPTIMIZATION_CONTINUOUS_RANGE = {
     'Filler Proportion(%)': (0,85),
     'Temperature(K)': (273,573)
 }
-
-### Machine Learning
-MODEL_HIDDEN_LAYERS = [550,450,350,250,150,50]
-
-MODEL_INITIAL_LEARNING_RATE = 0.001
-
-MODEL_DROP_OUT_RATE = 0.2
-
-MODEL_TEST_SIZE = 0.2
-
-### Optimization
-OPTIMIZATION_HIDDEN_LAYERS = [120, 90, 60, 30]
-
-OPTIMIZATION_INITIAL_LEARNING_RATE = 0.001
-
-OPTIMIZATION_DROP_OUT_RATE = 0.2
-
-OPTIMIZATION_TEST_SIZE = 0.2
